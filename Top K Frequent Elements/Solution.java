bd1f46ac936b0c15ce276112aa20ad9425f5ebd8
matheusmedrado <matheusmedrado@ufu.br>
2025-05-28 19:31:48 -0300

    Top K Frequent

--- Top K Frequent Elements/Solution.java
+++ Top K Frequent Elements/Solution.java
@@ -1,12 +1,12 @@
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         // 1. Contar frequências
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
         for (int num : nums) {
             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
@@ -31,6 +31,43 @@ class Solution {
         }
         
         return result.stream().mapToInt(i -> i).toArray();
+    }
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int[] nums1 = {1, 1, 1, 2, 2, 3};
+        int k1 = 2;
+        int[] result1 = solution.topKFrequent(nums1, k1);
+        System.out.println("Test 1:");
+        System.out.println("Input: " + Arrays.toString(nums1) + ", k = " + k1);
+        System.out.println("Output: " + Arrays.toString(result1));
+        System.out.println();
+        
+        int[] nums2 = {1};
+        int k2 = 1;
+        int[] result2 = solution.topKFrequent(nums2, k2);
+        System.out.println("Test 2:");
+        System.out.println("Input: " + Arrays.toString(nums2) + ", k = " + k2);
+        System.out.println("Output: " + Arrays.toString(result2));
+        System.out.println();
+        
+
+        int[] nums3 = {1, 2, 3, 4, 5};
+        int k3 = 3;
+        int[] result3 = solution.topKFrequent(nums3, k3);
+        System.out.println("Test 3:");
+        System.out.println("Input: " + Arrays.toString(nums3) + ", k = " + k3);
+        System.out.println("Output: " + Arrays.toString(result3));
+        System.out.println();
+        
+
+        int[] nums4 = {4, 1, -1, 2, -1, 2, 3};
+        int k4 = 2;
+        int[] result4 = solution.topKFrequent(nums4, k4);
+        System.out.println("Test 4:");
+        System.out.println("Input: " + Arrays.toString(nums4) + ", k = " + k4);
+        System.out.println("Output: " + Arrays.toString(result4));
     }
 }
 
